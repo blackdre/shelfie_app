79c2b04229facd6907372c09e3248a6c64af97a4
Blessing mhlanga <42203958+blackdre@users.noreply.github.com>
2025-08-13 13:37:00 +0200

    added light and dark mode styling

--- my-app/app/quotations.jsx
+++ my-app/app/quotations.jsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, useColorScheme } from "react-native";
 import Logo from "../assets/adaptive-icon.png";
 import { Link } from "expo-router";
+import { Colors } from "../constants/Colors";
 
 const Quotations = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
       <Image source={Logo} style={{ width: 100, height: 100 }} />
       <Text style={styles.title}>The Number 1</Text>
       <Text style={{ marginTop: 10, marginBottom: 10 }}>Reading list app</Text>
